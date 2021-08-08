6b1c9945ddd838dfd5748305331a9fa199bbf543
João Pedro A <jaaopbr@gmail.com>
2021-08-08 20:09:19 -0300

    ✨ feat: updated modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ReactiveFormsModule } from "@angular/forms";
+
+// Material
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+
+// Components
 import { LoginComponent } from "./views/login/login.component";
-import { PaymentsComponent } from './views/payments/payments.component';
+import { PaymentsComponent } from "./views/payments/payments.component";
+import { LoginFormComponent } from "./shared/components/login-form/login-form.component";
+
 @NgModule({
-  declarations: [AppComponent, LoginComponent, PaymentsComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PaymentsComponent,
+    LoginFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
