1f8ceca5484e541d22a51f9a6721cd4bffdbf564
João Pedro A <jaaopbr@gmail.com>
2021-08-10 14:45:16 -0300

    ✨ feat: add get with pagination params

---
+++ src/app/shared/services/payments.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { IAccount } from "../interfaces/Account";
+
+import { IPayments } from "../interfaces/Payments";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PaymentsService {
+  baseUrl: string = "http://localhost:3000/tasks";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+    }),
+  };
+
+  constructor(public httpClient: HttpClient) {}
+
+  getPayments(page: number, limit: number): Observable<IPayments[]> {
+    let params = new HttpParams();
+    params = params.append("_page", String(page));
+    params = params.append("_limit", String(limit));
+
+    return this.httpClient.get<IPayments[]>(this.baseUrl, { params }).pipe(
+      map((payments: IPayments[]) => payments),
+      catchError((error) => throwError(error))
+    );
+  }
+}
