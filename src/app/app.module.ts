d332ca40894526202b23ecc393b4cfa0e8de3e0f
Camila Hatakeyama <hatakeyama.camila@gmail.com>
2021-08-11 19:55:36 -0300

    refactor(app.module): reorganiza os imports
    Organiza os imports por tipo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
 import ptBr from '@angular/common/locales/pt';
 
 import { MatTableModule } from '@angular/material/table';
@@ -18,25 +18,25 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { MeusPagamentosComponent } from './components/meus-pagamentos/meus-pagamentos.component';
-import { AppComponent } from './app.component';
 import { MatSortModule } from '@angular/material/sort';
-import { DateFormatPipe } from './pipes/date-format.pipe';
-
-registerLocaleData(ptBr);
-import { registerLocaleData } from '@angular/common';
-import { VisibleOnHoverDirective } from './directives/visible-on-hover.directive';
-import { DialogManagerComponent } from './components/meus-pagamentos/dialog-manager/dialog-manager.component';
-import { NgxCurrencyModule } from "ngx-currency";
-import { DialogTemplateComponent } from './components/meus-pagamentos/dialog-template/dialog-template.component';
-import { LoginComponent } from './components/login/login.component';
-import { HeaderComponent } from './components/template/header/header.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorIntlPt } from './share/mat-paginator-intl-pt';
+
+import { ToastrModule } from 'ngx-toastr';
+import { NgxCurrencyModule } from "ngx-currency";
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component';
+import { DateFormatPipe } from './pipes/date-format.pipe';
+import { HeaderComponent } from './components/template/header/header.component';
+import { DialogManagerComponent } from './components/meus-pagamentos/dialog-manager/dialog-manager.component';
+import { DialogTemplateComponent } from './components/meus-pagamentos/dialog-template/dialog-template.component';
+import { LoginComponent } from './components/login/login.component';
+import { MeusPagamentosComponent } from './components/meus-pagamentos/meus-pagamentos.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { VisibleOnHoverDirective } from './directives/visible-on-hover.directive';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
