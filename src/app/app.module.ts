a80e1f5a44d1a8b7668237748cd2ccf984362946
andersonvaler <andersonvaler@gmail.com>
2021-08-15 18:15:38 -0300

    feat: Add home page and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,26 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule } from "@angular/common/http";
 
-//COMPONENTS
-import { AppComponent } from "./app.component";
-import { LoginComponent } from "./pages/login/login.component";
-import { LoginFormComponent } from "./components/login-form/login-form.component";
-
-//MATERIAL
+//Material components
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
+//Aplication components
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { LoginFormComponent } from "./components/login-form/login-form.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, LoginFormComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LoginFormComponent,
+    HomeComponent,
+    HeaderComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
