2fc9d9e931d5ed3a9c929f3d8f11dce836202d07
andersonvaler <andersonvaler@gmail.com>
2021-08-15 18:07:26 -0300

    feat: Add routing module, material-ui, created component form-login and style init login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+//COMPONENTS
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { LoginFormComponent } from "./components/login-form/login-form.component";
+
+//MATERIAL
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
 @NgModule({
-  declarations: [	
-    AppComponent,
-   ],
+  declarations: [AppComponent, LoginComponent, LoginFormComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+
+    //MATERIAL
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
