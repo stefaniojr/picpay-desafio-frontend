ef584dbbb7360d237273c99cd662861330cecf84
Stefânio Jr <stefaniojr@live.com>
2021-08-20 19:27:34 -0300

    feat: define o path '' como payments, atualiza o favicon e o título.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: 'payments', loadChildren: () => import('./payments/payments.module').then(m => m.PaymentsModule) }];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule) },
+  { path: '', loadChildren: () => import('./payments/payments.module').then((m) => m.PaymentsModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
