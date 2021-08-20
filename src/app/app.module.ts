766835bd5e7adf8c05473bfec5b28bb9530a675e
Stefânio Jr <stefaniojr@live.com>
2021-08-20 18:40:17 -0300

    feat: cria app-routing.module.ts#Arquivo estava faltando, portanto foi necessário criá-lo automaticamente.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Routes } from '@angular/router'
+
+export const ROUTES: Routes = [];
+
 @NgModule({
   declarations: [	
     AppComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
